19c7c0b62297871086db6cc64ee6953c44803a36
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-06 17:23:32 +0200

    added a movie card

---
+++ frontend/src/components/MovieCard.tsx
@@ -0,0 +1,33 @@
+type Movie = {
+    url?: string;
+    title: string;
+    description: string;
+    type: string;
+    releaseDate: string;
+};
+
+function MovieCard(movie: Movie) {
+
+    function onFavoriteClick() {
+        alert('Favorite clicked!!');
+    }
+
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={ movie.url } alt={ movie.title } />
+                <div>
+                    <button className="favorite-btn" onClick={onFavoriteClick} >
+                        ❤️
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.releaseDate}</p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
