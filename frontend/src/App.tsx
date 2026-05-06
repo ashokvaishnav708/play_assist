c198a4b7baa30b2a3675c557e6abe089d876252c
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-06 21:06:33 +0200

    added router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import './App.css';
+import Favorites from './pages/Favorites';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router';
+import NavigationBar from './components/NavigationBAr';
 
 function App() {
 
   return (
     <div>
+      <NavigationBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
     </div>
   );
 }
