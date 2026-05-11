509a8745f4f9586e6618af398765443bf42d3732
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-11 11:45:30 +0200

    changed the port address

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,8 @@ DEBUG = True
 app = FastAPI(debug=DEBUG)
 
 origins = [
-    "http://localhost:5174",
-    "http://127.0.0.1:5174"
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
     ]
 
 app.add_middleware(CORSMiddleware,
