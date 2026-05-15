72a6e71334fd51c8aed9f97086bf89e1dde986ba
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-15 16:22:15 +0200

    fix: favorites page

--- frontend/src/pages/Favorites.tsx
+++ frontend/src/pages/Favorites.tsx
@@ -1,21 +1,34 @@
 import '../css/Favorites.css';
 import { useMovieContext } from '../contexts/MovieContext';
 import MovieCard from '../components/MovieCard';
+import type { Media } from '../services/types';
 
 function Favorites()  {
 
     const { favorites } = useMovieContext();
 
-    if (favorites) {
+    function getMovieCard(movie: Media) {
+        return <MovieCard 
+                    id={movie.id} 
+                    key={movie.id} 
+                    title={movie.title} 
+                    overview={movie.overview} 
+                    release_date={movie.release_date} 
+                    poster_path={movie.poster_path} 
+                    original_language={movie.original_language}
+                />;
+    }
+
+    if (favorites && favorites.length) {
         return (
             <div className='movies-grid'>
                 {
-                    // TODO: Add movie card component properly
-                    favorites.map((movie) => <MovieCard />)
+                    favorites.map((movie) => getMovieCard(movie))
                 }
             </div>
         );
     }
+
 
     return (
         <div className='favorites-empty'>
