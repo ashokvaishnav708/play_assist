02cd2ecc8e834afeb12438da460b939ba5bcd4e9
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-17 21:36:17 +0200

    added models

---
+++ backend/models/movies.py
@@ -0,0 +1,17 @@
+from typing import List
+from pydantic import BaseModel
+
+class Movie(BaseModel):
+    id: int
+    title: str
+    release_date: str
+    poster_path: str | None
+    original_language: str
+    overview: str
+
+class Movies(BaseModel):
+    movies: List[Movie]
+
+
+class IsLoaded(BaseModel):
+    is_loaded: bool
