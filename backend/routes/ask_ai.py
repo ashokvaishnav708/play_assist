dbff15ca1f8a13641b0dc4351cdfdb7ee454c762
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-28 14:34:00 +0200

    added query agent api interface

--- backend/routes/ask_ai.py
+++ backend/routes/ask_ai.py
@@ -3,6 +3,7 @@ from logging import getLogger
 
 from models.ask_ai import QueryRequest, QueryResponse
 from rag.rag_chain import rag_chain
+from agent.agent import movie_agent
 
 logger = getLogger(__name__)
 
@@ -15,3 +16,14 @@ async def query_ai(request: QueryRequest) -> QueryResponse:
     
     answer, movies = rag_chain.query(query)
     return QueryResponse(answer=answer, movies=movies)
+
+@router.post("/query_agent", response_model=QueryResponse)
+async def query_agent(request: QueryRequest) -> QueryResponse:
+    query = request.question
+    logger.debug(f"Agent query received: {query}")
+    movie_agent.query_agent(query)
+
+    return QueryResponse(answer="", movies=[])
+
+
+
