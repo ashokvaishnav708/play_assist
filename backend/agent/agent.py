36ae0465253a31e284f411762328a97376a7accc
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-29 11:17:37 +0200

    fix: prompt description

--- backend/agent/agent.py
+++ backend/agent/agent.py
@@ -16,7 +16,7 @@ def get_movies_via_genre(genre_ids: List[int]) -> List[Movie]:
     Gets all the movies matching genre ids.
 
     @param genre_ids: List of genre ids
-    @return: List of movies
+    @return: List of movies ids
     """
     movies = movie_store.get_movies()
 
@@ -28,7 +28,7 @@ def get_movies_via_genre(genre_ids: List[int]) -> List[Movie]:
 
 
 SYSTEM_MESSAGE = SystemMessage(content="""You are a helpful assistant for movies suggestions.
-                               Tool: {get_movies_via_genre}
+                               Tools: {get_movies_via_genre}
                                The genre and associated genre ids for movies are Action = 28, Adventure = 12, Animation = 16, Comedy = 35, Crime = 80, Documentary = 99, Drama = 18, Family = 10751, Fantasy = 14, History = 36, Horror = 27, Music = 10402, Mystery = 9648, Romance = 10749, Science Fiction (Sci-Fi) = 878, Thriller = 53, War = 10752, Western = 37.
                                If user queries for movie suggestions, get movie genre idea from query, collect genre ids and get movies via genre.
                                Observation: If list is empty the print: empty list, for eg: []. Else, print all the movies ids in the list format like [id1, id2, ...].
@@ -36,7 +36,7 @@ SYSTEM_MESSAGE = SystemMessage(content="""You are a helpful assistant for movies
 
 class MovieAgent:
     def __init__(self, gemini_api_key: str):
-        self.__llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", gemini_api_key=gemini_api_key, temperature=0.3)
+        self.__llm = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", google_api_key=gemini_api_key, temperature=0.3)
         self.__agent = create_agent(
             model=self.__llm,
             tools=[get_movies_via_genre],
@@ -47,10 +47,11 @@ class MovieAgent:
             "messages": [HumanMessage(content=query)]
         })
 
-        logger.info(f"Agent resposnse: {result['messages'][-1].content_blocks}")
+        logger.debug(f"Agent resposnse: {result['messages'][-1].content_blocks}")
+        logger.info(f"Messages {result['messages']}")
+        # Sample response from agent 
+        # [{'type': 'text', 'text': 'OK. Here are some action movies for you: [id1, id2, ...., id_n ].'}]
         
-
-
 
 
 env_path = find_dotenv()
