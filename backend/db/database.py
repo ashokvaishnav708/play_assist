a5e6e70f0f8de68767b496b552541a53cb79d2fa
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-05-29 18:49:35 +0200

    fix: db

--- backend/db/database.py
+++ backend/db/database.py
@@ -11,16 +11,11 @@ Base = declarative_base()
 
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
     # Enable pgvector
     with engine.connect() as conn:
         conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
         conn.commit()
-
+    Base.metadata.create_all(bind=engine)
 
 def get_db():
-    db: Session = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    return SessionLocal()
