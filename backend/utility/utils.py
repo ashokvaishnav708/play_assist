9a2ad01ec9d4b6fbd20d9750845d36d6bb86c689
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-06-01 18:25:35 +0200

    added default None to get env key

--- backend/utility/utils.py
+++ backend/utility/utils.py
@@ -4,7 +4,7 @@ from logging import getLogger
 logger = getLogger(__name__)
 
 
-def get_env_key(key: str, fallback_value: str | None) -> str | None:
+def get_env_key(key: str, fallback_value: str | None = None) -> str | None:
     value = os.getenv(key)
 
     if not value:
