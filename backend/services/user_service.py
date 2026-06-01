07473833076162f7b4211c48c0f33d87b601c082
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-06-01 16:55:41 +0200

    added user service

---
+++ backend/services/user_service.py
@@ -0,0 +1,32 @@
+from db.respository.user import UserRepository
+from models.user import UserCreateRequest, UserLoginRequest, UserWithToken, UserResponse
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from utility.security.hash_helper import HashHelper
+from utility.security.auth_handler import AuthHandler
+
+class UserService:
+    def __int__(self, session: Session):
+        self.__user_repo = UserRepository(session=session)
+
+    def signup(self, user_data: UserCreateRequest) -> UserResponse:
+        if self.__user_repo.get_user_by_email(user_data.email):
+            raise HTTPException(status_code=400, detail="User already exists, please login.")
+        
+        hashed_password = HashHelper.get_password_hash(plain_password=user_data.password)
+        user_data.password = hashed_password
+
+        return self.__user_repo.create_user(user_data=user_data)
+    
+    def login(self, login_details: UserLoginRequest) -> UserWithToken:
+        if not self.__user_repo.get_user_by_email(login_details.email):
+            raise HTTPException(status_code=400, detail="User does not exists, please signup to login.")
+        
+        user = self.__user_repo.get_user_by_email(email=login_details.email)
+
+        if HashHelper.verify_password(plain_password=login_details.password, hashed_password=user.password):
+            token = AuthHandler.sign_jwt(user_id=user.id)
+            if token:
+                raise UserWithToken(token=token)
+            raise HTTPException(status_code=500, detail="Unable to process request.")
+        raise HTTPException(status_code=400, detail="Please check your credentials.")
