c0d6ed0dd27dbff93bb6e946a23678cbe9199b91
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-06-26 11:39:04 +0200

    get embedding model based on query or not

--- backend/ai/llm.py
+++ backend/ai/llm.py
@@ -6,5 +6,6 @@ GOOGLE_API_KEY = get_env_key("GEMINI_API_KEY")
 def get_llm_model() -> ChatGoogleGenerativeAI:
     return ChatGoogleGenerativeAI(model="gemini-3-flash-preview", google_api_key=GOOGLE_API_KEY, temperature=0.1)
 
-def get_embedding_model() -> GoogleGenerativeAIEmbeddings:
-    return GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview", google_api_key=GOOGLE_API_KEY)
+def get_embedding_model(is_query: bool = False) -> GoogleGenerativeAIEmbeddings:
+    task_type = "retrieval_query" if is_query else "retrieval_document"
+    return GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview", google_api_key=GOOGLE_API_KEY, task_type=task_type)
