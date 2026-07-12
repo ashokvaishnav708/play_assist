0fa05afee54f7f09adb0810268680fb45f669beb
Ashok Vaishnav <ashok.askalsoft@gmail.com>
2026-07-12 16:08:19 +0200

    added token version in db for logouts

--- backend/db/schema.py
+++ backend/db/schema.py
@@ -46,5 +46,6 @@ class User(Base):
     email = Column(String(70), unique=True)
     password = Column(String(250))
     created_at = Column(DateTime, default=datetime.now, nullable=False)
+    token_version = Column(Integer, default=0, nullable=False)
     # favorite_movies = Column(ARRAY(UUID(as_uuid=True), dimensions=1), nullable=False)
     # favorites_tv_series = Column(ARRAY(UUID(as_uuid=True), dimensions=1), nullable=False)
